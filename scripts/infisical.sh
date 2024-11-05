fb0560896a75177517ba516e300b9f3108e65e86
Pierre-Adrien Vasseur <pierreadrien.vasseur@gmail.com>
2024-11-05 16:53:34 +0100

    fix: infisical

--- scripts/infisical.sh
+++ scripts/infisical.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+set -x 
+
 response=$(curl --silent --request GET \
   --url https://us.infisical.com/api/v3/secrets/raw \
-  --header 'Authorization: Bearer '$INFISICAL_API_KEY)
+  --header 'Authorization: Bearer '$INFISICAL_DEV)
 
 echo $response | jq -r '.secrets[] | "export \(.secretKey)=\(.secretValue)"' | while read -r line; do
   echo "Setting secret: $line"
